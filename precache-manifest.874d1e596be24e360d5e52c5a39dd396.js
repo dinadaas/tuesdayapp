d416650357bc4596fe3b94366f9b5ee007804b2d
Dina Daas <dina.daas@duke.edu>
2019-04-22 03:01:28 -0400

    Updates

---
+++ precache-manifest.874d1e596be24e360d5e52c5a39dd396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8b6abac77390f3305f138a4e9fa1336",
+    "url": "/tuesdayapp/static/media/KS.d8b6abac.png"
+  },
+  {
+    "revision": "3a7e97aeb8d45e7d5171",
+    "url": "/tuesdayapp/static/js/runtime~main.cf020312.js"
+  },
+  {
+    "revision": "0bc8ac1db86808dfc0c8",
+    "url": "/tuesdayapp/static/js/main.b60680b4.chunk.js"
+  },
+  {
+    "revision": "34ec1ebd498cbbde4079",
+    "url": "/tuesdayapp/static/js/2.1130de1f.chunk.js"
+  },
+  {
+    "revision": "0bc8ac1db86808dfc0c8",
+    "url": "/tuesdayapp/static/css/main.677009b1.chunk.css"
+  },
+  {
+    "revision": "34ec1ebd498cbbde4079",
+    "url": "/tuesdayapp/static/css/2.2c8c841f.chunk.css"
+  },
+  {
+    "revision": "1e8cfca4e2cb83d6e5013a8505dde935",
+    "url": "/tuesdayapp/index.html"
+  }
+];
